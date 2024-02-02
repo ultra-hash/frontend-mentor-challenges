5baac4c68b61dc944aefff05a27400229fde7ef9
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2024-02-02 11:52:38 +0530

    test

--- social-links-profile-main/src/App.jsx
+++ social-links-profile-main/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className='bg-secondary-cOff-black min-h-[100vh] flex justify-center items-center'>
       <div className='w-[80%] max-w-[310px] bg-secondary-cDark-grey p-4 rounded-lg text-secondary-cWhite flex flex-col gap-3'>
         <div className='text-center'>
-          <img className='m-auto rounded-full w-20' src='src\assets\images\avatar-jessica.jpeg' alt='jessica avatar' />
+          <img className='m-auto rounded-full w-20' src='public\assets\images\avatar-jessica.jpeg' alt='jessica avatar' />
           <h1 className="text-2xl text-cInter font-bold mb-1 mt-4">
             Jessica Randall
           </h1>
